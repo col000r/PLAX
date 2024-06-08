28f6e4527ba8ab943441b4cb2e5a6e6d58e1a588
Markus Hofer <markus@brightlight.rocks>
2024-06-08 12:45:06 +1200

    added status quo

---
+++ Assets/Plax/Plax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plax : MonoBehaviour {
+    [SerializeField] private Transform m_Control;
+    [SerializeField] private List<PlaxElement> m_PlaxElements = new List<PlaxElement>();
+
+    private void Start() {
+        foreach (var plaxElement in m_PlaxElements) {
+            plaxElement.Offset = plaxElement.Trans.localPosition;
+        }
+    }
+
+    void Update() {
+        Vector3 ctrlPos = m_Control.localPosition;
+        foreach (var plaxElement in m_PlaxElements) {
+            float val = plaxElement.GetVal(ctrlPos.x);
+            plaxElement.Trans.localPosition = new Vector3(ctrlPos.x * val, ctrlPos.y * val,
+                                                  plaxElement.Trans.localPosition.z) + plaxElement.Offset;
+        }
+    }
+}
+
+[Serializable]
+public class PlaxElement {
+    public Transform Trans;
+    public float Val;
+    public bool UseCurve;
+    public AnimationCurve XValCurve;
+    public Vector3 Offset;
+
+    public float GetVal(float x) {
+        if (UseCurve) {
+            return XValCurve.Evaluate(x);
+        }
+
+        return Val;
+    }
+    
+    //TODO: move behind mid point when reaching a certain spot
+    //TODO: switch sprites when reaching certain values?
+}
---
+++ Assets/Plax/PlaxControlMouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaxControlMouse : MonoBehaviour {
+    [SerializeField] private Transform m_PlaxTransform;
+	[SerializeField] private Vector2 m_LimitHorizontal = new Vector2(-0.1f, 0.1f);
+	[SerializeField] private Vector2 m_LimitVertical = new Vector2(-0.1f, 0.1f);
+    void Update() {
+	    m_PlaxTransform.position = new Vector3(
+		    Mathf.Lerp( m_LimitHorizontal[0], m_LimitHorizontal[1], Mathf.InverseLerp( 0f, Screen.width, Input.mousePosition.x ) ),
+		    Mathf.Lerp(  m_LimitVertical[0], m_LimitVertical[1], Mathf.InverseLerp( 0f, Screen.height, Input.mousePosition.y ) ),
+		    0f );
+    }
+}
